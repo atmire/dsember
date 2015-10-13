772f1d4c16730fa960dd5f65a7eecc3c24eb8e17
Art Lowel <art@ArtMac.local>
2015-10-13 17:23:35 +0200

    first full item display page

--- app/router.js
+++ app/router.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import config from './config/environment';
+import coreMap from './utils/route-setup';
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  coreMap(this);
 });
 
 export default Router;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,5 +19,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  //app.import('bower_components/blueimp-md5/js/md5.min.js');
+
   return app.toTree();
 };
