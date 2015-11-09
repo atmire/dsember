a8953153525b65fa5a35a5536b31770a17053656
Art Lowel <art@ArtMac.local>
2015-11-09 13:22:50 +0100

    Fixed a bunch of JS-lint errors, and added a 404 page

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import config from './config/environment';
 import coreMap from './utils/route-setup';
+import fallbackMap from './utils/fallback-route-setup';
 
 var Router = Ember.Router.extend({
   location: config.locationType
@@ -8,6 +9,10 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   coreMap(this);
+
+  //this map contains the fallback routes,
+  //and should therefore be the last one
+  fallbackMap(this);
 });
 
 export default Router;
